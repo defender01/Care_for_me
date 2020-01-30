eec415ab38984dc9337a564ee6a3e4a5be15b76b
defendet01 <r.r.rahman.r.r@gmail.com>
2020-01-30 21:24:27 +0600

    med history updated

--- public/jsFiles/medHistory.js
+++ public/jsFiles/medHistory.js
@@ -85,21 +85,68 @@ function togglePopUp(name) {
     }
   }
 
-  document.getElementById('radio_yes').checked = false;
+  function togglePopUpFamilyHistory(from) {
+    if(from === "parents")
+        document.getElementById('conditional').style.display = 'none'
+    else if(from === 'ok'){
+            
+        document.getElementById('conditional').style.display = 'inline'
+    }
+    if (document.getElementById('popup').style.display == "flex") {
+        document.getElementById('popup').style.display = "none";
+    } else {
+        document.getElementById('popup').style.display = "flex";
+    }
+}
   
 function changeButtonText(id)
 {
     var elem = document.getElementById(id)
-    var x= document.getElementById(id).innerHTML
-    alert(x)
     if (elem.innerText=="See more") elem.innerText = "See less";
     else elem.innerText = "See more";
 }
 
-window.onload = function ()
+function displayOccupationDetails(id){
+
+    var elem = document.getElementById(id)
+    var el = document.getElementById("employmentDetails")
+    var uel = document.getElementById("unemploymentDetails")
+   
+    if(id == "employed" && elem.checked){
+        el.style.display = "block"
+        uel.style.display = "none"
+    }
+    else {
+        el.style.display = "none"
+        uel.style.display = "block"
+    }
+}
+
+function addVaccineDetails(){
+    var parent = document.getElementById("vaccineDetails")
+    var newDiv = document.createElement("div")
+    parent.appendChild(newDiv)
+    newDiv.outerHTML = '<div class="row" id= "vaccine">'+
+    '<div class="col-7">'+
+      '<input type="text" name="vaccine" placeholder="Vaccine name" />'+
+    '</div>'+
+    '<div class="col-5">'+
+      '<input type="text" name="date_taken" placeholder="Date" />'+
+    '</div>'+
+  '</div>'
+  var el= document.querySelectorAll('vaccine')
+}
+
+function onStart()
 {
     var items = document.querySelectorAll('[id="seeMore"]')
-    alert(items.length)
+    // keep occupation details of step4 hidden at the beginning
+    var el = document.getElementById("employmentDetails")
+    var uel = document.getElementById("unemploymentDetails")
+    el.style.display="none"
+    uel.style.display="none"
+   
+ 
     for(var i=0; i<items.length; i++){
         items[i].id='seeMore'+i.toString()
     }
