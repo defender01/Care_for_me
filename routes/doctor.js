6a9630dd6e37742aa834b6f7421381b5f42d0ab8
defendet01 <r.r.rahman.r.r@gmail.com>
2020-07-09 20:42:22 +0600

    followup ques

--- routes/doctor.js
+++ routes/doctor.js
@@ -1,13 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
+const {
+  checkAuthenticated,
+  checkNotAuthenticated,
+} = require("../controllers/auth_helper");
 
 const {
     followupQuesModel 
   } = require("../models/followup");
 
-router.get('/followupQues', (req, res) => {
-    res.render('followupQues')
+router.get('/followupQues',checkAuthenticated, (req, res) => {
+let displayName = req.user.name.displayName;
+    res.render('followupQues', {displayName})
 })
   
 module.exports = router;
