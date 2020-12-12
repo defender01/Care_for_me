610ac9b41eb230a2394388df7a3e2b78cfee8cf1
defendet01 <r.r.rahman.r.r@gmail.com>
2020-12-12 20:20:20 +0600

    follow up ques continue page selected ques show with values

--- routes/doctor.js
+++ routes/doctor.js
@@ -24,11 +24,28 @@ router.post('/followupQues/continue', checkAuthenticatedDoctor, checkEmailVerifi
   let data = req.body
   let qIds = data.questionId
   let questions = []
-  if(Array.isArray(qIds)){
-    for(let i=0; i< qIds.length; i++){
-      let qId = qIds[i]
+  if(typeof qIds!== 'undefined'){
+    if(Array.isArray(qIds)){
+      for(let i=0; i< qIds.length; i++){
+        let qId = qIds[i]
+        let question = {
+          id : qId,
+          inputType : data['inputType'+qId],
+          name : data['name'+qId],
+          startDate : data['startDate'+qId],
+          endDate : data['endDate'+qId],
+          frequency : data['frequency'+qId],
+          maxVal : data['maxVal'+qId],
+          minVal : data['minVal'+qId],
+        }
+        questions.push(question)
+      }    
+    }
+    else{
+      let qId = qIds
       let question = {
         id : qId,
+        inputType : data['inputType'+qId],
         name : data['name'+qId],
         startDate : data['startDate'+qId],
         endDate : data['endDate'+qId],
@@ -37,21 +54,9 @@ router.post('/followupQues/continue', checkAuthenticatedDoctor, checkEmailVerifi
         minVal : data['minVal'+qId],
       }
       questions.push(question)
-    }    
-  }
-  else{
-    let qId = qIds
-    let question = {
-      id : qId,
-      name : data['name'+qId],
-      startDate : data['startDate'+qId],
-      endDate : data['endDate'+qId],
-      frequency : data['frequency'+qId],
-      maxVal : data['maxVal'+qId],
-      minVal : data['minVal'+qId],
     }
-    questions.push(question)
   }
+  
   console.log(util.inspect({questions}, false, null, true /* enable colors */))
   res.render('followUpQuesContinue', {questions, navDisplayName})
 })
