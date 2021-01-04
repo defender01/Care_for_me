207eb01567540d3366789e01f5649048a02a69aa
defendet01 <r.r.rahman.r.r@gmail.com>
2021-01-04 19:53:43 +0600

    patient record page

--- routes/doctor.js
+++ routes/doctor.js
@@ -73,6 +73,11 @@ router.get('/patients', (req, res) => {
   res.render('patients', {navDisplayName})
 })
 
+router.get('/patients/records', (req, res) => {
+  let navDisplayName = req.user.name.displayName;
+  res.render('patientRecords', {navDisplayName})
+})
+
 // this provides new id
 router.get('/getNewId', (req, res) => {
   res.send({ id: new mongoose.Types.ObjectId() })
